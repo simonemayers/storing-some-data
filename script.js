f7602b2e5c655c47bdc025959482db19771698b9
Simone Mayers <53239742+simonemayers@users.noreply.github.com>
2022-03-22 09:57:23 -0400

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+let group = ["Simone", "Poonam", "Choua", "Mark", "Cameron"]
+let food =["fruits", "vegetables", "beans", "grains", "nuts", "seeds", "baked goods", "legumes", "herbs", "spices"]
+let states = [
+    ["Alabama", "Montgomery", "Yellowhammer"], 
+    ["Alaska", "Juneau", "Willow Ptarmigan"], 
+    ["Arizona", "Phoenix", "Cactus Wren"], 
+    ["Arkansas", "Little Rock", "Mockingbird"], 
+    ["California", "Sacramento", "California Valley Quail"], 
+    ["Colorado", "Denver", "Lark Bunting"], 
+    ["Connecticut", "Hartford", "American Robin"]
+]
+console.log(group, food, states)
